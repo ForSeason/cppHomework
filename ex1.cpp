4182d1e2872d6b0d8f0a23650754b60eecc85a2b
forseason <ysy604822@live.com>
2019-04-09 21:16:17 +0800

    优化了ex1的算法

--- ex1.cpp
+++ ex1.cpp
@@ -2,65 +2,80 @@
 #include <sstream>
 #include <regex>
 #include <string>
+#include <typeinfo>
 #include <cmath>
 using namespace std;
 
-int char_n2dec(char in) {
-    char dict[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-    for (int i = 0; i < 16; i++) if (dict[i] == in) return i;
-    return 1;
-}
+class Solution {
 
-string char_dec2n(int in) {
-    string dict[16] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
-    return dict[in];
-}
+    protected:
+        int char_n2dec(char in) {
+            return ('A' <= in && in <= 'Z')? (int)(in - 'A' + 10): (int)(in - '0');
+        }
 
-string n2dec(string in, int n) {
-    int sum = 0, len = (int)in.length();
-    for (int i = 0; i < len; i++) sum += char_n2dec(in[len - i - 1]) * pow(n, i);
-    return to_string(sum);
-}
+        string char_dec2n(long int in) {
+            char tmp;
+            string res;
+            tmp = (in < 10)? (char)('0' + in): (char)('A' + in - 10);
+            stringstream sss;
+            sss << tmp;
+            return sss.str();
+        }
 
-string dec2n(string in, int n) {
-    string res = "";
-    int temp;
-    istringstream iss(in);
-    iss >> temp;
-    do {
-        res = char_dec2n(temp % n) + res;
-        temp /= n;
-    } while (temp);
-    return res;
-}
+        string n2dec(string in, int n) {
+            long int sum = 0;
+            int len = (int)in.length();
+            for (int i = 0; i < len; i++) sum += char_n2dec(in[len - i - 1]) * pow(n, i);
+            return to_string(sum);
+        }
 
-bool check_input(string in_str, int in, int out) {
-    if (!(2 <= in && in <= 16) || !(2 <= out && out <= 16)) {
-        return false;
-    } else {
-        string dict = "0123456789ABCDEF";
-        regex pattern("[" + dict.substr(0, in) + "]+");
-        return regex_match(in_str, pattern);
-    }
-}
+        string dec2n(string in, int n) {
+            string res = "";
+            long int temp;
+            istringstream iss(in);
+            iss >> temp;
+            do {
+                res   = char_dec2n(temp % n) + res;
+                temp /= n;
+            } while (temp);
+            return res;
+        }
 
-void input(int& in, int& out, string& in_str) {
-    cout << "input metadata: ";
-    getline(cin, in_str);
-    cout << "input original system: ";
-    cin >> in;
-    cout << "input target system: ";
-    cin >> out;
-}
+        bool check_input(string in_str, int in, int out) {
+            if (!(2 <= in && in <= 36) || !(2 <= out && out <= 36)) {
+                return false;
+            } else {
+                string dict = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                regex pattern("[" + dict.substr(0, in) + "]+");
+                return regex_match(in_str, pattern);
+            }
+        }
+
+        void input(string& in_str, int& in, int& out) {
+            cout << "请输入要转换的数字(最大36进制, 仅支持正整数): ";
+            getline(cin, in_str);
+            cout << "请输入原本数字的进制: ";
+            cin  >> in;
+            cout << "请输入要输入的数字的进制: ";
+            cin  >> out;
+        }
+
+    public:
+        int run() {
+            int in, out;
+            string in_str;
+            input(in_str, in, out);
+            if (check_input(in_str, in, out)) {
+                cout << "结果: " << dec2n(n2dec(in_str, in), out) << endl;
+                return 0;
+            } else {
+                cout << "非法输入!" << endl;
+                return 403;
+            }
+        }
+};
 
 int main() {
-    int in, out;
-    string in_str;
-    input(in, out, in_str);
-    if (check_input(in_str, in, out)) {
-        cout << "Result: " << dec2n(n2dec(in_str, in), out) << endl;
-    } else {
-        cout << "Invalid input!" << endl;
-    }
-    return 0;
+    Solution sol;
+    return sol.run();
 }
